51d1dfe29223e0df447fc406c0eb437599d64fd3
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-07-30 12:43:01 +0300

    test

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Diary from './Diary';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-        <Diary />
-    </BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<Header />} /> */}
+      <Route path="/" element={<Diary />} />
+    </Routes>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './NavBar.css';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
     <nav className="menu">
-      <a className="menu__item menu__item_active" href="#">Домой</a>
-      <a className="menu__item" href="#">Советы</a>
+      <NavLink to="/" className={({isActive}) => `menu__item ${isActive ? "menu__item_active" : ""}`}>Домой</NavLink>
+      <NavLink to="/tips" className={({isActive}) => `menu__item ${isActive ? "menu__item_active" : ""}`}>Советы</NavLink>
     </nav>
   );
 }
