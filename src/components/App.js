eb37421e4966781b0207bb20d44b4df44dcd867b
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-07-30 23:06:23 +0300

    работа с формами

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Diary from './Diary';
-// import { BrowserRouter } from 'react-router-dom';
+import Tips from './Tips';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
 
-import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      {/* <Route path="/" element={<Header />} /> */}
-      <Route path="/" element={<Diary />} />
-    </Routes>
+    <BrowserRouter>
+      <Header />
+      <main className="content">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Diary />} />
+          <Route path="/tips" element={<Tips />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
