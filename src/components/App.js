612eb45f42477e63c96ddd80ab98e6200b8276a5
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-08-11 22:57:12 +0300

    Защита маршрутов на фронте

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import Header from './Header';
 import Diary from './Diary';
 import Tips from './Tips';
@@ -7,39 +7,34 @@ import Register from './Register';
 import Login from './Login';
 import NavBar from './NavBar';
 import './styles/App.css';
+import ProtectedRouteElement from "./ProtectedRoute";
+
 
 class App extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-      loggedIn: false 
-    };
+      loggedIn: false
+    }
   }
-
-  render() {
-    const { loggedIn } = this.state;
-
+  render(){
     return (
-      <BrowserRouter>
-        <Header />
-        <main className="content">
-          {this.state.loggedIn && <NavBar />}
-          <Routes>
-            <Route
-              path="/"
-              element={
-                this.state.loggedIn ? <Navigate to="/diary" /> : <Navigate to="/login" />
-              }
-            />
-            <Route path="/diary" element={<Diary />} />
-            <Route path="/tips" element={<Tips />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    );
+    <BrowserRouter>
+      <Header />
+      <main className="content">
+        {this.state.loggedIn && <NavBar />}
+        <Routes>
+          <Route path="/" element={this.state.loggedIn ? <Navigate to="/diary" replace /> : <Navigate to="/login" replace />} />
+          <Route path="/diary" element={<ProtectedRouteElement element={Diary} loggedIn={this.state.loggedIn}/>} />
+          <Route path="/tips" element={<ProtectedRouteElement element={Tips} loggedIn={this.state.loggedIn}/>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
   }
 }
+
 
 export default App;
